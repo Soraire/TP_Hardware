a38f5bad0113bf189ee092154da182b7c281bd9d
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-11-10 12:07:54 -0300

    falta guardar video

--- src/Funcionalidades/VideoPlayer.js
+++ src/Funcionalidades/VideoPlayer.js
@@ -1,58 +1,58 @@
-/*
-NEEDS
-ASYNC
-STORAGE
-https://react-native-async-storage.github.io/async-storage/docs/install
-*/
 import { Video, ResizeMode } from 'expo-av';
 import React from 'react';
 import { View, StyleSheet, Button, TextInput, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage'; // Asegúrate de importar AsyncStorage
 
 const VideoPlayer = () => {
   const video = React.useRef(null);
   const [status, setStatus] = React.useState({});
   const [videoLink, setVideoLink] = React.useState('');
-  
+
   const handleVideoSubmit = async () => {
     try {
       await AsyncStorage.setItem('videoLink', videoLink);
       console.log('Enlace del video guardado con éxito');
     } catch (error) {
       console.error('Error al guardar el enlace del video:', error);
-    } 
+    }
+  };
+
+  const handlePlayPause = () => {
+    status.isPlaying ? video.current.pauseAsync() : video.current.playAsync();
   };
 
   return (
     <View style={styles.container}>
-      <Text/><Text/><Text/><Text/>
+      <Text />
+      <Text />
+      <Text />
+      <Text />
       <TextInput
         style={styles.input}
         placeholder="Enter Video Link"
-        value={video} 
-        onChangeText={(value) => setVideoLink(value)}
-        onSubmitEditing={handleVideoSubmit}
+        value={videoLink}
       />
-      <View style={styles.container}>
-      <Video
-        ref={video}
-        style={styles.video}
-        source={{
-          uri: video,
-        }}
-        useNativeControls
-        resizeMode={ResizeMode.CONTAIN}
-        isLooping
-        onPlaybackStatusUpdate={status => setStatus(() => status)}
-      />
-      <View style={styles.buttons}>
-        <Button
-          title={status.isPlaying ? 'Pause' : 'Play'}
-          onPress={() =>
-            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
-          }
+      <Button title="Submit" onPress={handleVideoSubmit} />
+
+      <View style={styles.videoContainer}>
+        <Video
+          ref={video}
+          style={styles.video}
+          source={{
+            uri: videoLink, // Usar el enlace que ingresó el usuario
+          }}
+          useNativeControls
+          resizeMode={ResizeMode.CONTAIN}
+          isLooping
+          onPlaybackStatusUpdate={(status) => setStatus(() => status)}
         />
+        <View style={styles.buttons}>
+          <Button
+            title={status.isPlaying ? 'Pause' : 'Play'}
+            onPress={handlePlayPause}
+          />
+        </View>
       </View>
-    </View>
     </View>
   );
 };
@@ -79,6 +79,9 @@ const styles = StyleSheet.create({
   video: {
     width: 300,
     height: 200,
+  },
+  buttons: {
+    marginTop: 10,
   },
 });
 
